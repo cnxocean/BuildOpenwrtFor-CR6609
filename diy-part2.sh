3cf44c1b21c6f6cf5b63e57dc0bd6e315d35e66b
cnxocean <xocean@126.com>
2022-11-24 13:58:08 +0800

    add wifi defout enable

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,3 +15,6 @@ sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generat
 # Change default theme
 sed -i 's/default Bootstrap theme/default Argon theme/g' feeds/luci/collections/luci/Makefile
 sed -i 's/+luci-theme-bootstrap/+luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# Change wifi default enable
+sed -i 's/.disabled=1/.disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
